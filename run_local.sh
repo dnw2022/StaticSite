46af1184cd34eafc02b56848d7676a959a810c94
Jeroen Bijlsma <jeroen_bijlsma@yahoo.com>
2022-04-12 18:24:45 +0200

    Add index.html page

--- run_local.sh
+++ run_local.sh
@@ -2,4 +2,4 @@
 docker build -t staticsite -f ./1.0/Dockerfile ./1.0
 
 # Run container
-docker run -it --rm -d -p 8080:80 --name staticsite staticsite
+docker run -it --rm -d -p 8080:8080 --name staticsite staticsite
